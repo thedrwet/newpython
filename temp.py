47a70990cc91c4951fa64036bbbd4eed8bd899d9
thedrwet <140302450+thedrwet@users.noreply.github.com>
2024-03-20 15:57:26 +0530

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import random
+x='y'
+while x=='y':
+    no=random.randint(1, 6)
+    if no==1:
+        print("1")
+    if no==2:
+        print("2")
+    if no==3:
+        print("3")
+    if no==4:
+        print("4")
+    if no==5:
+        print("5")
+    if no==6:
+        print("6")
+    x=input("press y to roll again and n for exit")
+    print()
